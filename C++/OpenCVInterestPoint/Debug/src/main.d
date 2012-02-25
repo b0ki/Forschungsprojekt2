eb95842bb22230e54082faa260f0d329d7fa8d32
Christian Bunk <christianb.github@googlemail.com>
2012-02-26 00:07:59 +0100

    set up OpenCV in Eclipse as a native application

---
+++ C++/OpenCVInterestPoint/Debug/src/main.d
@@ -0,0 +1,7 @@
+src/main.d src/main.o: ../src/main.cpp \
+  /usr/local/Cellar/opencv/2.3.1a/include/opencv/cv.h \
+  /usr/local/Cellar/opencv/2.3.1a/include/opencv/highgui.h
+
+/usr/local/Cellar/opencv/2.3.1a/include/opencv/cv.h:
+
+/usr/local/Cellar/opencv/2.3.1a/include/opencv/highgui.h:
