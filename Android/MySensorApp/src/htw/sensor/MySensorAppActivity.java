1623c29655894325cc71597bc484c8dd9e9c0b6b
Christian Bunk <christianb.github@googlemail.com>
2012-03-07 17:00:10 +0100

    invert rotation matrix to get values from world system

--- Android/MySensorApp/src/htw/sensor/MySensorAppActivity.java
+++ Android/MySensorApp/src/htw/sensor/MySensorAppActivity.java
@@ -1,12 +1,14 @@
 package htw.sensor;
 
 import android.app.Activity;
+import android.graphics.Matrix;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.TextView;
 
 public class MySensorAppActivity extends Activity implements SensorEventListener {
 	
@@ -24,6 +26,14 @@ public class MySensorAppActivity extends Activity implements SensorEventListener
 
 	private Sensor mAccelerometer;
 	private float[] mGravity;
+	
+	private TextView mTextRotationMatrix;
+
+	private TextView mTextRotationVector;
+
+	private Sensor mRotationVector;
+
+	private float[] mRotation;
 
 	/** Called when the activity is first created. */
     @Override
@@ -39,6 +49,90 @@ public class MySensorAppActivity extends Activity implements SensorEventListener
         mLinearAcceleration = mSensorManager.getDefaultSensor(Sensor.TYPE_LINEAR_ACCELERATION);
         mMagneticField = mSensorManager.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD);
         mAccelerometer = mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        
+        mRotationVector = mSensorManager.getDefaultSensor(Sensor.TYPE_ROTATION_VECTOR);
+        
+        
+        /*
+        float[] input = new float[3];
+        float[][] rotationMatrix = new float[3][3];
+        
+        input[0] = 1;
+        input[1] = 2;
+        input[2] = 3;
+        
+        rotationMatrix[0][0] = 2;
+        rotationMatrix[0][1] = 3;
+        rotationMatrix[0][2] = 4;
+        
+        rotationMatrix[1][0] = 3;
+        rotationMatrix[1][1] = 4;
+        rotationMatrix[1][2] = 5;
+        
+        rotationMatrix[2][0] = 4;
+        rotationMatrix[2][1] = 5;
+        rotationMatrix[2][2] = 6;
+        
+        float[] result = matrixMultiply(input, rotationMatrix);
+        Log.d(LOG_TAG, "Matrix multiplication:");
+        Log.d(LOG_TAG, result[0] +" "+ result[1] + " " + result[2]);
+        */
+        
+        mTextRotationMatrix = (TextView) findViewById(R.id.text_rotation_matrix);
+        mTextRotationVector = (TextView) findViewById(R.id.text_rotation_vector);
+        
+        /*float value = 0.924f;
+        float rounded = Math.round(value*100)/100f;
+        mLightInfo.setText("gerundet: "+roundNDigitsAfterComma(value, 0));
+        //mLightInfo.setText("gerundet: "+round1DigitAfterComma(value));*/
+        Matrix m = new Matrix();
+        float[] values = new float[9];
+        
+        float[] point = new float[9];
+        point[0] = 9;
+        point[1] = 0;
+        point[2] = 0;
+        point[3] = 0;
+        point[4] = 9;
+        point[5] = 0;
+        point[6] = 0;
+        point[7] = 0;
+        point[8] = 9;
+        m.setValues(point);
+        
+        m.getValues(values);
+        
+        Matrix invertMatrix = new Matrix(m);
+        //float[] inverted = new float[9];
+        Log.d(LOG_TAG, "invert matrix: " + m.invert(m));
+        m.getValues(values);
+        printRotationMatrix(values);
+    }
+    
+    /**
+     * Rundet eine Zahl auf n Stellen nach dem Komma.
+     * @param value Zu rundende Zahl.
+     * @param n Stelle bis zu der nach dem Komma gerundet werden soll. n > 0
+     * @return Gerundete Zahl.
+     */
+    private float roundNDigitsAfterComma(float value, int n) {
+    	if (n <= 0) {
+    		return Float.MIN_NORMAL;
+    	}
+    	
+    	float exp = (float) Math.pow(10, n);
+
+    	return Math.round(value*exp)/exp;
+    	
+    }
+    
+    private float[] roundNDigitsAfterComma(float[] values, int size, int n) {
+    	float result[] = new float[size];
+    	for (int i = 0; i < size; i++) {
+    		result[i] = roundNDigitsAfterComma(values[i], n);
+    	}
+    	
+    	return result;
     }
     
     protected void onResume() {
@@ -47,6 +141,9 @@ public class MySensorAppActivity extends Activity implements SensorEventListener
         //mSensorManager.registerListener(this, mPressure, SensorManager.SENSOR_DELAY_FASTEST);
         //mSensorManager.registerListener(this, mProximity, SensorManager.SENSOR_DELAY_FASTEST);
         //mSensorManager.registerListener(this, mLinearAcceleration, SensorManager.SENSOR_DELAY_NORMAL);
+        
+        mSensorManager.registerListener(this, mRotationVector, SensorManager.SENSOR_DELAY_NORMAL);
+        
         mSensorManager.registerListener(this, mMagneticField, SensorManager.SENSOR_DELAY_NORMAL);
         mSensorManager.registerListener(this, mAccelerometer, SensorManager.SENSOR_DELAY_NORMAL);
     }
@@ -67,6 +164,7 @@ public class MySensorAppActivity extends Activity implements SensorEventListener
 		// TODO Auto-generated method stub
 		if (Sensor.TYPE_LIGHT == event.sensor.getType()) {
 			Log.d(LOG_TAG, "light values" + event.values[0]);
+			mTextRotationMatrix.setText(new Float(event.values[0]).toString());
 		}
 		
 		if (Sensor.TYPE_PRESSURE == event.sensor.getType()) {
@@ -85,43 +183,120 @@ public class MySensorAppActivity extends Activity implements SensorEventListener
 		}
 		
 		if (Sensor.TYPE_MAGNETIC_FIELD == event.sensor.getType()) {
-			/*Log.d(LOG_TAG, "magnetic field values[0]" + event.values[0]);
-			Log.d(LOG_TAG, "magnetic field values[1]" + event.values[1]);
-			Log.d(LOG_TAG, "magnetic field values[2]" + event.values[2]);*/
-			mGeomagnetic = event.values;
+			
+			//Log.d(LOG_TAG, "Magnetfeldwerte (x,y,z): ");
+			//Log.d(LOG_TAG, event.values[0] + " " + event.values[1] + " " + event.values[2]);
+			mGeomagnetic = event.values.clone();
 		}
 		
 		if (Sensor.TYPE_ACCELEROMETER == event.sensor.getType()) {
-			mGravity = event.values;
+			mGravity = event.values.clone();
+		}
+		
+		if (Sensor.TYPE_ROTATION_VECTOR == event.sensor.getType()) {
+			mRotation = event.values.clone();
 		}
 		
 		float[] R = new float[9];
 		float[] I = new float[9];
+		//float[] rotation = new float[16];
+		//float[] orientVals = new float[3];
+		//final float pi = (float) Math.PI;
+		//final float rad2deg = 180/pi;
 		
-		if (mGravity != null && mGeomagnetic != null) {
+		/*if (mGravity != null && mGeomagnetic != null) {
 			boolean result = SensorManager.getRotationMatrix(R, I, mGravity, mGeomagnetic);
-			//Log.d(LOG_TAG, "getRotationMatrix() : "+result);	
 			
 			if (result) {
-				Log.d(LOG_TAG, "R[0] = "+R[0]);
-				Log.d(LOG_TAG, "R[1] = "+R[1]);
-				Log.d(LOG_TAG, "R[2] = "+R[2]);
-				Log.d(LOG_TAG, "R[3] = "+R[3]);
-				Log.d(LOG_TAG, "R[4] = "+R[4]);
-				Log.d(LOG_TAG, "R[5] = "+R[5]);
-				Log.d(LOG_TAG, "R[6] = "+R[6]);
-				Log.d(LOG_TAG, "R[7] = "+R[7]);
-				Log.d(LOG_TAG, "R[8] = "+R[8]);
+				// rundet Matrix auf n Stellen nach dem Komma
+				R = roundNDigitsAfterComma(R, 9, 1);
+				printRotationMatrix(R);
 				
-				float[] actual_orientation = new float[3];
-			    SensorManager.getOrientation(R, actual_orientation);
-			    
-			    Log.d(LOG_TAG, "actual_orientation[0] = "+actual_orientation[0]);
-			    Log.d(LOG_TAG, "actual_orientation[1] = "+actual_orientation[1]);
-			    Log.d(LOG_TAG, "actual_orientation[2] = "+actual_orientation[2]);
+				// Multipliziere Rotationsmatrix mit Gravity Vektor
+				float[] rotatedVector = matrixMultiply(mGeomagnetic, transformRotationMatrix(R));
+				printVector(rotatedVector, "rotated gravity vector");
 			}
 			
+		}*/
+		
+		if (mRotation != null && mGravity != null) {
+			SensorManager.getRotationMatrixFromVector(R, mRotation);
+			R = roundNDigitsAfterComma(R, 9, 1);
+			Matrix rotationMatrix = new Matrix();
+			rotationMatrix.setValues(R);
+			Log.d(LOG_TAG, "is invertable? "+rotationMatrix.invert(rotationMatrix));
+			rotationMatrix.getValues(R);
+			printRotationMatrix(R);
+			
+			// Multipliziere Rotationsmatrix mit Gravity Vektor
+			float[] rotatedVector = matrixMultiply(mGravity, transformRotationMatrix(R));
+			//printVector(roundNDigitsAfterComma(mGravity, 3, 1), "rotated gravity vector");
+			printVector(roundNDigitsAfterComma(rotatedVector, 3, 1), "rotated gravity vector");
 		}
 		
+	}
+	
+	/** Print a Vektor with 3 elements
+	 * 
+	 * @param input
+	 */
+	private void printVector(float[] input, String name) {
+		StringBuffer buf = new StringBuffer();
+		buf.append("( "+ input[0] + " " + input[1] + " " + input[2] + ")");
+		//Log.d(LOG_TAG, name);
+		//Log.d(LOG_TAG, "( "+ input[0] + " " + input[1] + " " + input[2] + ")");
+		mTextRotationVector.setText(buf.toString());
+	}
+	
+	/**
+	 * Multipliziert eine 1x3 Matrix mit einer 3x3 Matrix..
+	 * @param input [spalte]
+	 * @param rotationMatrix float[zeile][spalte]
+	 * @return
+	 */
+	private float[] matrixMultiply(float[] input, float[][] rotationMatrix) {
+		float[] result = new float[3];
+		for (int spalte = 0; spalte < 3; spalte++) {
+			int sum = 0;
+			for (int zeile = 0; zeile < 3; zeile++) {
+				sum += input[zeile] * rotationMatrix[zeile][spalte];
+			}
+			
+			result[spalte] = sum;
+		}
+		
+		return result;
+	}
+	
+	private float[][] transformRotationMatrix(float[] originRotationMatrix) {
+		float[][] result = new float[3][3];
+		
+		result[0][0] = originRotationMatrix[0];
+		result[0][1] = originRotationMatrix[1];
+		result[0][2] = originRotationMatrix[2];
+		result[1][0] = originRotationMatrix[3];
+		result[1][1] = originRotationMatrix[4];
+		result[1][2] = originRotationMatrix[5];
+		result[2][0] = originRotationMatrix[6];
+		result[2][1] = originRotationMatrix[7];
+		result[2][2] = originRotationMatrix[8];
+		
+		return result;
+	}
+	
+	private void printRotationMatrix(float[] R) {
+		StringBuffer buf = new StringBuffer();
+		buf.append(R[0] + " " + R[1] + " " + R[2] + "\n");
+		buf.append(R[3] + " " + R[4] + " " + R[5] + "\n");
+		buf.append(R[6] + " " + R[7] + " " + R[8]);
+		
+		mTextRotationMatrix.setText(buf.toString());
+		
+		/*
+		Log.d(LOG_TAG, "Rotation Matrix: ");
+		Log.d(LOG_TAG, "| "+ R[0] + " " + R[1] + " " + R[2] + "Ê|");
+		Log.d(LOG_TAG, "| "+ R[3] + " " + R[4] + " " + R[5] + "Ê|");
+		Log.d(LOG_TAG, "| "+ R[6] + " " + R[7] + " " + R[8] + "Ê|");
+		*/
 	}
 }
